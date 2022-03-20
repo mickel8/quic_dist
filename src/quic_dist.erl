66f6db366dd464270872c7cb1524e53cf9567a34
Michał Śledź <sledzson08@gmail.com>
2022-03-20 22:18:49 +0100

    Implement `listen` function

--- src/quic_dist.erl
+++ src/quic_dist.erl
@@ -3,11 +3,31 @@
 -export([listen/1, listen/2, address/0, accept/1, accept_connection/5, setup/5, close/1,
          select/1, setopts/2, getopts/2]).
 
-listen(_Name) ->
-    ok.
+-include_lib("kernel/include/net_address.hrl").
+-include_lib("kernel/include/logger.hrl").
 
-listen(_Name, _Host) ->
-    ok.
+listen(Name) ->
+    {ok, Host} = inet:gethostname(),
+    listen(Name, Host).
+
+listen(Name, Host) ->
+    ?LOG_INFO("Ping"),
+    erlang:display("Ping"),
+    application:ensure_all_started(quicer),
+    {MinPort, _MaxPort} = get_port_range(),
+    LOptions = [{cert, "cert.pem"}, {key, "key.pem"}, {alpn, ["sample"]}],
+    {ok, L} = quicer:listen(MinPort, LOptions),
+    {ok, {Address, _Port}} = quicer:sockname(L),
+    NetAddress =
+        #net_address{address = Address,
+                     host = Host,
+                     protocol = udp,
+                     family = inet},
+    Creation = 1,
+    {ok, {L, NetAddress, Creation}}.
+
+get_port_range() ->
+    {50000, 60000}.
 
 address() ->
     ok.
@@ -25,7 +45,8 @@ close(_Listen) ->
     ok.
 
 select(_NodeName) ->
-    ok.
+    % TODO
+    true.
 
 setopts(_Listen, _Opts) ->
     ok.
